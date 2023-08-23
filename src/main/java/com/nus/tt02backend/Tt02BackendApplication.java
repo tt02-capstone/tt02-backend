f0554b94f2cd83a81a04ab3902764f61bf082030
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-08-23 10:40:47 +0800

    initial commit

---
+++ src/main/java/com/nus/tt02backend/Tt02BackendApplication.java
@@ -0,0 +1,13 @@
+package com.nus.tt02backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tt02BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tt02BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nus/tt02backend/Tt02BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nus.tt02backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tt02BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
