acbfe8d3f3f0bfaa6dd27265d563691428064abb
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-06 20:44:17 +0800

    Refactor methods

--- src/main/java/com/nus/tt02backend/repositories/VendorStaffRepository.java
+++ src/main/java/com/nus/tt02backend/repositories/VendorStaffRepository.java
@@ -2,6 +2,11 @@ package com.nus.tt02backend.repositories;
 
 import com.nus.tt02backend.models.VendorStaff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VendorStaffRepository extends JpaRepository<VendorStaff, Long> {
+    @Query("SELECT vs FROM VendorStaff vs WHERE vs.email=?1")
+    VendorStaff retrieveVendorStaffByEmail(String email);
 }
