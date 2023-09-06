a5e5d2df2c57538aa1874267731156478c16659a
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-06 16:57:21 +0800

    Update create method

--- src/main/java/com/nus/tt02backend/repositories/VendorStaffRepository.java
+++ src/main/java/com/nus/tt02backend/repositories/VendorStaffRepository.java
@@ -1,8 +1,7 @@
 package com.nus.tt02backend.repositories;
 
-import com.nus.tt02backend.models.Vendor;
 import com.nus.tt02backend.models.VendorStaff;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VendorRepository extends JpaRepository<Vendor, Long> {
+public interface VendorStaffRepository extends JpaRepository<VendorStaff, Long> {
 }
