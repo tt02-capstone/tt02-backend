e2a9cc07d6cd448651e337271136b558c86a644d
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-09-06 16:57:21 +0800

    Update create method

--- src/main/java/com/nus/tt02backend/services/impl/VendorServiceImpl.java
+++ src/main/java/com/nus/tt02backend/services/impl/VendorServiceImpl.java
@@ -2,8 +2,10 @@ package com.nus.tt02backend.services.impl;
 
 import com.nus.tt02backend.exceptions.BadRequestException;
 import com.nus.tt02backend.exceptions.NotFoundException;
+import com.nus.tt02backend.models.Vendor;
 import com.nus.tt02backend.models.VendorStaff;
 import com.nus.tt02backend.repositories.VendorRepository;
+import com.nus.tt02backend.repositories.VendorStaffRepository;
 import com.nus.tt02backend.services.VendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +16,8 @@ import java.util.List;
 
 @Service
 public class VendorServiceImpl implements VendorService {
+    @Autowired
+    VendorStaffRepository vendorStaffRepository;
     @Autowired
     VendorRepository vendorRepository;
     PasswordEncoder encoder = new BCryptPasswordEncoder();
@@ -35,7 +39,7 @@ public class VendorServiceImpl implements VendorService {
     }
 
     public void updateVendor(VendorStaff vendorStaffToUpdate) throws NotFoundException {
-        VendorStaff vendorStaff = vendorRepository.findById((vendorStaffToUpdate.getUser_id()))
+        VendorStaff vendorStaff = vendorStaffRepository.findById((vendorStaffToUpdate.getUser_id()))
                 .orElseThrow(() -> new NotFoundException("VendorStaff not found"));
 
         if (vendorStaff.getEmail().equals(vendorStaffToUpdate.getEmail())) {
@@ -44,24 +48,28 @@ public class VendorServiceImpl implements VendorService {
             }
         }
 
-        vendorRepository.save(vendorStaff);
+        vendorStaffRepository.save(vendorStaff);
     }
 
     public Long createVendor(VendorStaff vendorStaffToCreate) throws BadRequestException {
         List<VendorStaff> vendorStaffs = retrieveAllVendors();
-
         for (VendorStaff vendorStaff : vendorStaffs) {
             if (vendorStaff.getEmail().equals(vendorStaffToCreate.getEmail())) {
-                throw new BadRequestException("VendorStaff email exists");
+                throw new BadRequestException("The email address has been used, please enter another email");
             }
         }
 
+        Vendor vendorToCreate = vendorStaffToCreate.getVendor();
+        vendorRepository.save(vendorToCreate);
+
+        //vendorStaffToCreate.setVendor(vendorToCreate);
         vendorStaffToCreate.setPassword(encoder.encode(vendorStaffToCreate.getPassword()));
-        vendorRepository.save(vendorStaffToCreate);
+        vendorStaffRepository.save(vendorStaffToCreate);
+
         return vendorStaffToCreate.getUser_id();
     }
 
     public List<VendorStaff> retrieveAllVendors() {
-        return vendorRepository.findAll();
+        return vendorStaffRepository.findAll();
     }
 }
