65ec64a34fe3f6bc59aa76ee77d70a6d35a3ef0c
Leah James <60879267+rosejtec@users.noreply.github.com>
2023-09-13 21:39:39 +0800

    Leah/tourist login (#2)
    "Add User controller, rep and services"

---
+++ src/main/java/com/nus/tt02backend/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nus.tt02backend.repositories;
+
+import com.nus.tt02backend.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email=?1 and (u.user_type = 'TOURIST' or u.user_type = 'LOCAL')")
+    User retrieveTouristOrLocalByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.password_reset_token=?1 and (u.user_type = 'TOURIST' or u.user_type = 'LOCAL')")
+    User retrieveTouristOrLocalByToken(String token);
+}
