826ec9946fed2152e10fac3c4cbe8aab42798aa9
darryl <darrylgoh51@gmail.com>
2023-09-24 00:15:40 +0800

    fixed bugs

--- src/main/java/com/nus/tt02backend/repositories/PaymentRepository.java
+++ src/main/java/com/nus/tt02backend/repositories/PaymentRepository.java
@@ -8,16 +8,16 @@ import java.math.BigDecimal;
 
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
-    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status = 'COMPLETED' AND p.booking.attraction.attraction_id = ?1")
+    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status != 'CANCELLED' AND p.booking.attraction.attraction_id = ?1")
     Double retrieveSumOfBookingByAttractionId(Long id);
 
-    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status = 'COMPLETED' AND  p.booking.telecom.telecom_id = ?1")
+    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status != 'CANCELLED' AND  p.booking.telecom.telecom_id = ?1")
     Double retrieveSumOfBookingByTelecomId(Long id);
 
-    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status = 'COMPLETED' AND  p.booking.room.room_id = ?1")
+    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status != 'CANCELLED' AND  p.booking.room.room_id = ?1")
     Double retrieveSumOfBookingByRoomId(Long id);
 
-    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status = 'COMPLETED' AND  p.booking.deal.deal_id = ?1")
+    @Query("SELECT COALESCE(SUM(p.payment_amount), 0) FROM Payment p WHERE p.is_paid = true AND p.booking.status != 'CANCELLED' AND  p.booking.deal.deal_id = ?1")
     Double retrieveSumOfBookingByDealId(Long id);
 
     @Query("SELECT COALESCE(SUM(p.payment_amount),0) FROM Payment p WHERE p.is_paid = true AND p.booking.status = 'COMPLETED' AND p.booking.type = 'TOUR' AND p.booking.tour.tour_id = ?1")
