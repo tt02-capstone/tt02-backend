978f50083035e8c8c26953659add89463e45c32e
Gabriel Lee <gabriel.lee@u.nus.edu>
2023-10-16 20:58:47 +0800

    Create a parent/child new/reply comment

--- src/main/java/com/nus/tt02backend/controllers/CommentController.java
+++ src/main/java/com/nus/tt02backend/controllers/CommentController.java
@@ -1,10 +1,12 @@
 package com.nus.tt02backend.controllers;
 
+import com.nus.tt02backend.exceptions.BadRequestException;
+import com.nus.tt02backend.models.Comment;
+import com.nus.tt02backend.models.Post;
 import com.nus.tt02backend.services.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -13,4 +15,11 @@ public class CommentController {
     @Autowired
     CommentService commentService;
 
+    @PostMapping("/createComment/{postId}/{parentCommentId}/{userId}")
+    public ResponseEntity<Comment> createPost(@PathVariable Long postId, @PathVariable Long parentCommentId,
+                                           @PathVariable Long userId, @RequestBody Comment commentToCreate)
+            throws BadRequestException {
+        Comment comment = commentService.createComment(postId, parentCommentId, userId, commentToCreate);
+        return ResponseEntity.ok(comment);
+    }
 }
--- src/main/java/com/nus/tt02backend/services/CommentService.java
+++ src/main/java/com/nus/tt02backend/services/CommentService.java
@@ -1,8 +1,15 @@
 package com.nus.tt02backend.services;
 
+import com.nus.tt02backend.exceptions.BadRequestException;
+import com.nus.tt02backend.models.*;
+import com.nus.tt02backend.models.enums.UserTypeEnum;
 import com.nus.tt02backend.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -16,5 +23,114 @@ public class CommentService {
     PostRepository postRepository;
     @Autowired
     CommentRepository commentRepository;
+    @Autowired
+    VendorStaffRepository vendorStaffRepository;
+    @Autowired
+    InternalStaffRepository internalStaffRepository;
 
+    public Comment createComment(Long postId, Long parentCommentId, Long userId, Comment commentToCreate)
+            throws BadRequestException {
+        // Retrieve post
+        Optional<Post> postOptional = postRepository.findById(postId);
+        if (postOptional.isEmpty()) {
+            throw new BadRequestException("Post does not exist!");
+        }
+        Post post = postOptional.get();
+
+        // Retrieve parent comment
+        Comment parentComment = null;
+        if (parentCommentId != 0L) {
+            Optional<Comment> parentCommentOptional = commentRepository.findById(parentCommentId);
+            if (parentCommentOptional.isEmpty()) {
+                throw new BadRequestException("Parent comment does not exist!");
+            }
+            parentComment = parentCommentOptional.get();
+        }
+
+        // Retrieve user
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
+            throw new BadRequestException("User does not exist!");
+        }
+        User user = userOptional.get();
+
+        // Set the default and necessary values
+        if (parentComment != null) {
+            commentToCreate.setParent_comment(parentComment);
+        } else {
+            commentToCreate.setParent_comment(null);
+        }
+        commentToCreate.setPost(post);
+        commentToCreate.setPublish_time(LocalDateTime.now());
+        commentToCreate.setUpdated_time(LocalDateTime.now());
+        commentToCreate.setChild_comment_list(new ArrayList<>());
+        commentToCreate.setUpvoted_user_id_list(new ArrayList<>());
+        commentToCreate.setDownvoted_user_id_list(new ArrayList<>());
+        commentToCreate.setReported_comment_list(new ArrayList<>());
+        commentToCreate.setReply_list(new ArrayList<>());
+        Comment comment = commentRepository.save(commentToCreate);
+
+        // Add comment to post's comment list
+        post.getComment_list().add(comment);
+        postRepository.save(post);
+
+        // Add child comment to parent comment
+        if (parentComment != null) {
+            parentComment.getChild_comment_list().add(comment);
+            commentRepository.save(parentComment);
+        }
+
+        // Add comment to user's comment list
+        if (user.getUser_type().equals(UserTypeEnum.TOURIST)) {
+            Tourist tourist = (Tourist) user;
+            tourist.getComment_list().add(comment);
+            touristRepository.save(tourist);
+
+            comment.setTourist_user(tourist);
+            commentRepository.save(comment);
+
+            comment.getTourist_user().setPost_list(null);
+            comment.getTourist_user().setComment_list(null);
+        } else if (user.getUser_type().equals(UserTypeEnum.LOCAL)) {
+            Local local = (Local) user;
+            local.getComment_list().add(comment);
+            localRepository.save(local);
+
+            comment.setLocal_user(local);
+            commentRepository.save(comment);
+
+            comment.getLocal_user().setPost_list(null);
+            comment.getLocal_user().setComment_list(null);
+        } else if (user.getUser_type().equals(UserTypeEnum.VENDOR_STAFF)) {
+            VendorStaff vendorStaff = (VendorStaff) user;
+            vendorStaff.getComment_list().add(comment);
+            vendorStaffRepository.save(vendorStaff);
+
+            comment.setVendor_staff_user(vendorStaff);
+            commentRepository.save(comment);
+
+            comment.getVendor_staff_user().setPost_list(null);
+            comment.getVendor_staff_user().setComment_list(null);
+            comment.getVendor_staff_user().getVendor().setVendor_staff_list(null);
+        } else if (user.getUser_type().equals(UserTypeEnum.INTERNAL_STAFF)) {
+            InternalStaff internalStaff = (InternalStaff) user;
+            internalStaff.getComment_list().add(comment);
+            internalStaffRepository.save(internalStaff);
+
+            comment.setInternal_staff_user(internalStaff);
+            commentRepository.save(comment);
+
+            comment.getInternal_staff_user().setPost_list(null);
+            comment.getInternal_staff_user().setComment_list(null);
+        }
+
+        comment.getPost().setComment_list(null);
+        comment.getPost().setTourist_user(null);
+        comment.getPost().setLocal_user(null);
+        comment.getPost().setVendor_staff_user(null);
+        comment.getPost().setInternal_staff_user(null);
+        comment.setParent_comment(null);
+
+        return comment;
+    }
 }
