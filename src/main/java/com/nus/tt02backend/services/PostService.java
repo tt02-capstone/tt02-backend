921cc95b558bc3a2a459b401c78395465a5cf352
Ci En <ciensim@gmail.com>
2023-10-18 17:36:40 +0800

    unbind rs with booking

--- src/main/java/com/nus/tt02backend/services/PostService.java
+++ src/main/java/com/nus/tt02backend/services/PostService.java
@@ -188,9 +188,11 @@ public class PostService {
                 p.setComment_list(null);
                 if (p.getLocal_user() != null) {
                     p.getLocal_user().setPost_list(null);
+                    p.getLocal_user().setBooking_list(null);
                 }
                 else if (p.getTourist_user() != null) {
                     p.getTourist_user().setPost_list(null);
+                    p.getTourist_user().setBooking_list(null);
                 }
                 else if (p.getInternal_staff_user() != null) {
                     p.getInternal_staff_user().setPost_list(null);
@@ -217,9 +219,11 @@ public class PostService {
 
             if (p.getLocal_user() != null) {
                 p.getLocal_user().setPost_list(null);
+                p.getLocal_user().setBooking_list(null);
             }
             else if (p.getTourist_user() != null) {
                 p.getTourist_user().setPost_list(null);
+                p.getTourist_user().setBooking_list(null);
             }
             else if (p.getInternal_staff_user() != null) {
                 p.getInternal_staff_user().setPost_list(null);
