592258a17cdef64a36d8b1a7fd5dbc4f2e249fe7
rosejtec <rosej.tec@gmail.com>
2023-10-26 03:13:09 +0800

    Create Random Deals

--- src/main/java/com/nus/tt02backend/repositories/VendorRepository.java
+++ src/main/java/com/nus/tt02backend/repositories/VendorRepository.java
@@ -13,6 +13,9 @@ public interface VendorRepository extends JpaRepository<Vendor, Long> {
     @Query("SELECT v FROM Vendor v WHERE v.application_status=?1")
     List<Vendor> retrievePendingVendorApplications(ApplicationStatusEnum applicationStatus);
 
+    @Query("SELECT v.vendor_id FROM Vendor v")
+    List<Long> getAllVendorId();
+
     @Query("SELECT v.stripe_account_id FROM Vendor v WHERE v.business_name=?1")
     String getStripeIdByName(String business_name);
 
