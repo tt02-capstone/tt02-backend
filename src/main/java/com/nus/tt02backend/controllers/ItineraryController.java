13a594279a406b9e74fdbb0e8d15327beac08bb1
kellyco540 <kellyco540@gmail.com>
2023-10-26 12:41:42 +0800

    itinerary crud

--- src/main/java/com/nus/tt02backend/controllers/ItineraryController.java
+++ src/main/java/com/nus/tt02backend/controllers/ItineraryController.java
@@ -1,7 +1,8 @@
 package com.nus.tt02backend.controllers;
 
 import com.nus.tt02backend.exceptions.BadRequestException;
-import com.nus.tt02backend.models.DIYEvent;
+import com.nus.tt02backend.exceptions.NotFoundException;
+import com.nus.tt02backend.models.Itinerary;
 import com.nus.tt02backend.services.DIYEventService;
 import com.nus.tt02backend.services.ItineraryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,4 +16,27 @@ public class ItineraryController {
     @Autowired
     ItineraryService itineraryService;
 
+    @GetMapping("/getItineraryByUser/{userId}")
+    public ResponseEntity<Itinerary> getItineraryByUser(@PathVariable Long userId) throws NotFoundException, BadRequestException {
+        Itinerary itinerary = itineraryService.getItineraryByUser(userId);
+        return ResponseEntity.ok(itinerary);
+    }
+
+    @PostMapping("/createItinerary/{userId}")
+    public ResponseEntity<Itinerary> createItinerary(@PathVariable Long userId, @RequestBody Itinerary itineraryToCreate) throws BadRequestException {
+        Itinerary itinerary = itineraryService.createItinerary(userId, itineraryToCreate);
+        return ResponseEntity.ok(itinerary);
+    }
+
+    @PutMapping("/updateItinerary/{itineraryId}")
+    public ResponseEntity<Itinerary> updateItinerary(@PathVariable Long itineraryId, @RequestBody Itinerary itineraryToUpdate) throws IllegalArgumentException, NotFoundException, BadRequestException {
+        Itinerary itinerary = itineraryService.updateItinerary(itineraryId, itineraryToUpdate);
+        return ResponseEntity.ok(itinerary);
+    }
+
+    @DeleteMapping ("/deleteItinerary/{userId}/{itineraryId}")
+    public ResponseEntity<Void> deleteItinerary(@PathVariable Long userId, @PathVariable Long itineraryId) throws NotFoundException, BadRequestException {
+        itineraryService.deleteItinerary(userId, itineraryId);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/nus/tt02backend/services/ItineraryService.java
+++ src/main/java/com/nus/tt02backend/services/ItineraryService.java
@@ -1,10 +1,20 @@
 package com.nus.tt02backend.services;
 
-import com.nus.tt02backend.repositories.LocalRepository;
-import com.nus.tt02backend.repositories.TouristRepository;
-import com.nus.tt02backend.repositories.UserRepository;
+import com.nus.tt02backend.exceptions.BadRequestException;
+import com.nus.tt02backend.exceptions.NotFoundException;
+import com.nus.tt02backend.models.*;
+import com.nus.tt02backend.models.enums.InternalRoleEnum;
+import com.nus.tt02backend.models.enums.SupportTicketTypeEnum;
+import com.nus.tt02backend.models.enums.UserTypeEnum;
+import com.nus.tt02backend.repositories.*;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItineraryService {
@@ -14,5 +24,102 @@ public class ItineraryService {
     TouristRepository touristRepository;
     @Autowired
     LocalRepository localRepository;
+    @Autowired
+    ItineraryRepository itineraryRepository;
+    @Autowired
+    DIYEventRepository diyEventRepository;
+
+    public Itinerary getItineraryByUser(Long userId) throws BadRequestException {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
+            throw new BadRequestException("User does not exist!");
+        }
+        User user = userOptional.get();
+
+        Itinerary itineraryToReturn = null;
+
+        if (user.getUser_type().equals(UserTypeEnum.TOURIST)) {
+            Tourist tourist = (Tourist) user;
+            itineraryToReturn = tourist.getItinerary();
+        } else if (user.getUser_type().equals(UserTypeEnum.LOCAL)) {
+            Local local = (Local) user;
+            itineraryToReturn = local.getItinerary();
+        }
+        return itineraryToReturn;
+    }
+
+    public Itinerary createItinerary(Long userId, Itinerary itineraryToCreate) throws BadRequestException {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
+            throw new BadRequestException("User does not exist!");
+        }
+        User user = userOptional.get();
+
+        itineraryToCreate.setDiy_event_list(new ArrayList<>());
+
+        Itinerary itinerary = itineraryRepository.save(itineraryToCreate);
+
+        if (user.getUser_type().equals(UserTypeEnum.TOURIST)) {
+            Tourist tourist = (Tourist) user;
+            tourist.setItinerary(itinerary);
+            touristRepository.save(tourist);
+
+        } else if (user.getUser_type().equals(UserTypeEnum.LOCAL)) {
+            Local local = (Local) user;
+            local.setItinerary(itinerary);
+            localRepository.save(local);
+
+        }
+        return itinerary;
+    }
+
+    public Itinerary updateItinerary(Long itineraryId, Itinerary itineraryToUpdate) throws BadRequestException {
+        Optional<Itinerary> itineraryOptional = itineraryRepository.findById(itineraryId);
+        if (itineraryOptional.isEmpty()) {
+            throw new BadRequestException("Itinerary does not exist!");
+        }
+        Itinerary itinerary = itineraryOptional.get();
+
+        itinerary.setStart_date(itineraryToUpdate.getStart_date());
+        itinerary.setEnd_date(itineraryToUpdate.getEnd_date());
+        itinerary.setNumber_of_pax(itineraryToUpdate.getNumber_of_pax());
+        itinerary.setRemarks(itineraryToUpdate.getRemarks());
+
+        itinerary = itineraryRepository.save(itinerary);
+
+        return itinerary;
+    }
+
+    public void deleteItinerary(Long userId, Long itineraryId) throws NotFoundException, BadRequestException {
+
+        Optional<Itinerary> itineraryOptional = itineraryRepository.findById(itineraryId);
+        if (itineraryOptional.isEmpty()) {
+            throw new BadRequestException("Itinerary does not exist!");
+        }
+        Itinerary itinerary = itineraryOptional.get();
+
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isEmpty()) {
+            throw new BadRequestException("User does not exist!");
+        }
+        User user = userOptional.get();
+
+        if (user.getUser_type().equals(UserTypeEnum.TOURIST)) {
+            Tourist tourist = (Tourist) user;
+            tourist.setItinerary(null);
+            touristRepository.save(tourist);
+        } else if (user.getUser_type().equals(UserTypeEnum.LOCAL)) {
+            Local local = (Local) user;
+            local.setItinerary(null);
+            localRepository.save(local);
+        }
+
+        List<DIYEvent> diyEventList = itinerary.getDiy_event_list();
+        for (DIYEvent diyEvent : diyEventList) {
+            diyEventRepository.delete(diyEvent);
+        }
+
+        itineraryRepository.delete(itinerary);
+    }
 
 }
