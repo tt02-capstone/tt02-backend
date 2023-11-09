04867359501ca81176587f140b2d1768d1cfe34d
Ci En <ciensim@gmail.com>
2023-11-10 02:09:08 +0800

    get saved shopping items

--- src/main/java/com/nus/tt02backend/services/ItemService.java
+++ src/main/java/com/nus/tt02backend/services/ItemService.java
@@ -3,6 +3,7 @@ package com.nus.tt02backend.services;
 import com.nus.tt02backend.exceptions.BadRequestException;
 import com.nus.tt02backend.exceptions.NotFoundException;
 import com.nus.tt02backend.models.*;
+import com.nus.tt02backend.models.enums.UserTypeEnum;
 import com.nus.tt02backend.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -180,6 +181,26 @@ public class ItemService {
             }
         } else {
             throw new NotFoundException("User or item is not found!");
+        }
+    }
+
+    public List<Item> getUserSavedItems(Long userId) throws NotFoundException {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (user instanceof Tourist) {
+                Tourist t = (Tourist) user;
+                if (t.getItem_list() == null) return new ArrayList<>();
+                return t.getItem_list();
+            } else if (user instanceof Local) {
+                Local l = (Local) user;
+                if (l.getItem_list() == null) return new ArrayList<>();
+                return l.getItem_list();
+            } else {
+                throw new NotFoundException("User is not tourist or local!");
+            }
+        } else {
+            throw new NotFoundException("User not found!");
         }
     }
 }
